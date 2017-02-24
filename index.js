7caa4c8af0812f737c349c7b2c3d95a2ed29e1d6
Narendra Vaghela <narendravaghela4389@gmail.com>
2017-02-24 18:49:28 +0530

    Changes

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 #! /usr/bin/env node
 
-console.log('This is the demo script.');
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .command('new [directory_name]', 'install one or more packages')
+  .action(function (cmd, dir) {
+     cmdValue = cmd;
+     dirValue = dir;
+  });
+
+program.parse(process.argv);
+
+if (typeof cmdValue === 'undefined') {
+   console.error('no command given!');
+   process.exit(1);
+}
+
+console.log('command:', cmdValue);
+console.log('directory:', dirValue || "no directory given");
+process.exit(0);
